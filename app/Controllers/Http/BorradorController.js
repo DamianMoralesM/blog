b98de90ff458d806959f62b9e9185448a3ff0992
unknown <damian_morales_utn@hotmail.com>
2019-01-24 13:40:30 -0300

    image upload working

--- app/Controllers/Http/BorradorController.js
+++ app/Controllers/Http/BorradorController.js
@@ -1,4 +1,5 @@
 'use strict'
+const Helpers = use('Helpers')
 
 class BorradorController {
 
@@ -6,15 +7,38 @@ class BorradorController {
 
     }
 
-    store({request}){
-        const all = request.all() 
-        let aux =  all.bloque[0].split('\r\n')
+async  store({request}){
+       const all = request.all()
+      
+     //   let aux =  all.bloque[0].split('\r\n')
 
       /*  aux = (all.bloque).map(p => {
             p.split('\n')
         }) */
 
-        return aux 
+        // return aux
+
+        /**
+         This creates file, needs a name a validations rules as parameters
+         */
+        const profilePic = request.file('profile_pic', {
+            types: ['image'],
+            size: '2mb'
+          })
+        
+          /** creates de path */
+         
+          await profilePic.move(Helpers.tmpPath('uploads'), {
+            name: `${new Date().getTime()}.${profilePic.subtype}`,
+            overwrite: true
+          })
+        
+          /** finally moves to directory */
+          if (!profilePic.moved()) {
+            return profilePic.error()
+          }
+          return  all
+
     
     }
 
